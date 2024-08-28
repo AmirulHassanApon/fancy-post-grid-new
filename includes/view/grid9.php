901ba7e25d515d8cd98dea20937407cd57866d81
AmirulHassanApon <rstheme.apon@gmail.com>
2024-08-28 16:01:29 +0600

    Update grid9.php

--- includes/view/grid9.php
+++ includes/view/grid9.php
@@ -7,9 +7,10 @@ ob_start();
     <div class="container">
         <div class="row">
 
-            <?php
+        <?php
+            // =======Pagination==========
+            
             // Check if pagination is on or off
-            $fpg_post_per_page = -1;
             if ($fancy_post_pagination === 'off') {
                 $fpg_post_per_page = -1;
             }  
@@ -110,8 +111,8 @@ ob_start();
                 if (!empty($tax_query)) {
                     $args['tax_query'] = $tax_query;
                 }
-                // echo '<pre>' . print_r($args, true) . '</pre>';
-
+                
+                
             $query = new WP_Query($args);
 
             // Loop through the custom query
@@ -131,8 +132,10 @@ ob_start();
                 // Determine if the feature image should be hidden
                 $hide_feature_image = isset($fancy_post_hide_feature_image) && $fancy_post_hide_feature_image === 'off';
                 
+
                 // Determine the feature image size
-                $feature_image_size = isset($fancy_post_feature_image_size) ? (string) $fancy_post_feature_image_size : 'large';  
+                $feature_image_size = isset($fancy_post_feature_image_size) ? (string) $fancy_post_feature_image_size : '';  
+
                            
                 // Determine the media source
                 $media_source = isset($fancy_post_media_source) ? $fancy_post_media_source : 'feature_image';
@@ -154,59 +157,75 @@ ob_start();
 
                 // Apply hover animation class if needed
                 $hover_class = $hover_animation !== 'none' ? 'hover-' . esc_attr($hover_animation) : '';
-            ?>
+        ?>
 
-                    <div class="<?php echo esc_attr($main_cl_lg . ' ' .  $main_cl_md . ' ' . $main_cl_sm . ' ' . $main_cl_mobile); ?>">
-                        <div class="rs-blog-layout-16-item <?php echo esc_attr($hover_class); ?>">
-                            <?php if (!$hide_feature_image && $fpg_field_group_image) : ?>
-                                <div class="rs-thumb">
-                                    <?php if ($feature_image_url) : ?>
-                                        <a href="<?php the_permalink(); ?>" <?php echo $target_blank; ?>>
-                                            <img src="<?php echo esc_url($feature_image_url); ?>" alt="">
-                                        </a>
-                                    <?php endif; ?>
-                                </div>
+            <div class="<?php echo esc_attr($main_cl_lg . ' ' .  $main_cl_md . ' ' . $main_cl_sm . ' ' . $main_cl_mobile); ?>">
+                <div class="rs-blog-layout-16-item <?php echo esc_attr($main_alignment_class); ?> <?php echo esc_attr($hover_class); ?>">
+                    <!-- Image -->
+                    <?php if (!$hide_feature_image && $fpg_field_group_image) : ?>
+                        <div class="rs-thumb">
+                            <?php if ($feature_image_url) : ?>
+                                <a href="<?php the_permalink(); ?>" <?php echo $target_blank; ?>>
+                                    <img src="<?php echo esc_url($feature_image_url); ?>" alt="">
+                                </a>
                             <?php endif; ?>
-                            <div class="rs-content">
+                        </div>
+                    <?php endif; ?>
 
-                                <?php if ($fpg_field_group_title) : ?>
-                                    <<?php echo esc_attr($title_tag); ?> class="title">
-                                        <?php if ($fancy_link_details === 'on') : ?>
-                                            <a href="<?php the_permalink(); ?>"
-                                               <?php echo $target_blank; ?>
-                                               class="title-link">
-                                                <?php echo wp_trim_words(get_the_title(), $fancy_post_title_limit, $title_more_text); ?>
-                                            </a>
-                                        <?php else : ?>
-                                            <?php echo wp_trim_words(get_the_title(), $fancy_post_title_limit, $title_more_text); ?>
-                                        <?php endif; ?>
-                                    </<?php echo esc_attr($title_tag); ?>>
+                    <div class="rs-content">
+
+                        <!-- Title -->
+                        <?php if ($fpg_field_group_title) : ?>
+                            <<?php echo esc_attr($title_tag); ?> class="title <?php echo esc_attr($title_alignment_class); ?>" >
+                                <?php if ($fancy_link_details === 'on') : ?>
+                                    <a href="<?php the_permalink(); ?>"
+                                       <?php echo $target_blank; ?>
+                                       class="title-link">
+                                        <?php
+                                        if ($fancy_post_title_limit_type === 'words') {
+                                            echo wp_trim_words(get_the_title(), $fancy_post_title_limit, $title_more_text);
+                                        } elseif ($fancy_post_title_limit_type === 'characters') {
+                                            echo esc_html(mb_strimwidth(get_the_title(), 0, $fancy_post_title_limit, $title_more_text));
+                                        }
+                                        ?>
+                                    </a>
+                                <?php else : ?>
+                                    <?php
+                                    if ($fancy_post_title_limit_type === 'words') {
+                                        echo wp_trim_words(get_the_title(), $fancy_post_title_limit, $title_more_text);
+                                    } elseif ($fancy_post_title_limit_type === 'characters') {
+                                        echo esc_html(mb_strimwidth(get_the_title(), 0, $fancy_post_title_limit, $title_more_text));
+                                    }
+                                    ?>
                                 <?php endif; ?>
+                            </<?php echo esc_attr($title_tag); ?>>
+                        <?php endif; ?>
 
-                                <div class="rs-meta">
-                                    <div class="rs-author">
-                                        <?php echo get_avatar(get_the_author_meta('ID'), 32); ?>
-                                        <span>                                 
-                                            <?php the_author(); ?>
-                                        </span>
-                                    </div>
-                                    <div class="rs-date">
-                                        <span>
-                                            <i class="ri-time-line"></i> 
-                                            <?php echo get_the_date('d M Y'); ?>
-                                        </span>
-                                    </div>
-                                </div>
+                        <div class="rs-meta">
+                            <?php if ($fpg_field_group_author) : ?>
+                            <div class="rs-author">
+                                <?php echo get_avatar(get_the_author_meta('ID'), 32); ?>
+                                <span>                                 
+                                    <?php the_author(); ?>
+                                </span>
                             </div>
+                            <?php endif; ?>
+                            <?php if ($fpg_field_group_post_date) : ?>
+                            <div class="rs-date">
+                                <span>
+                                    <i class="ri-time-line"></i> 
+                                    <?php echo get_the_date('d M Y'); ?>
+                                </span>
+                            </div>
+                            <?php endif; ?>
                         </div>
                     </div>
-
+                </div>
+            </div>
             <?php
                 endwhile;
-                wp_reset_postdata(); // Reset post data to the main query
-            
+                wp_reset_postdata(); // Reset post data to the main query           
             ?>
-
         </div>
         <?php if ($fancy_post_pagination === 'on') : ?>
             <div class="fpg-pagination">
@@ -225,7 +244,181 @@ ob_start();
         <?php endif; ?>
     </div>
 </section>
+<style type="text/css">
+    /* General Styles */
+    .rs-blog-layout-16 {
+        <?php if (!empty($fpg_section_background_color)) : ?>
+            background-color: <?php echo esc_attr($fpg_section_background_color); ?>;
+        <?php endif; ?>
+        <?php if (!empty($fpg_section_margin)) : ?>
+            margin: <?php echo esc_attr($fpg_section_margin); ?>;
+        <?php endif; ?>
+        <?php if (!empty($fpg_section_padding)) : ?>
+            padding: <?php echo esc_attr($fpg_section_padding); ?>;
+        <?php endif; ?>
+    }
+    .rs-blog-layout-16 .rs-blog-layout-16-item{
+        <?php if (!empty($fpg_single_section_margin)) : ?>
+            margin: <?php echo esc_attr($fpg_single_section_margin); ?>;
+        <?php endif; ?>
+        <?php if (!empty($fpg_single_section_padding)) : ?>
+            padding: <?php echo esc_attr($fpg_single_section_padding); ?>;
+        <?php endif; ?>
 
+    }
+    .rs-blog-layout-16-item .rs-thumb{
+        <?php if (!empty($fancy_post_image_border_radius)) : ?>
+            border-radius: <?php echo esc_attr($fancy_post_image_border_radius); ?>;
+        <?php endif; ?>
+    }
+    /* Single Item Styles */
+    .rs-blog-layout-16-item .rs-content {
+        <?php if (!empty($fpg_single_section_background_color)) : ?>
+            background-color: <?php echo esc_attr($fpg_single_section_background_color); ?>;
+        <?php endif; ?>
+        
+        <?php if (!empty($fpg_single_section_border_color)) : ?>
+            border-color: <?php echo esc_attr($fpg_single_section_border_color); ?>;
+        <?php endif; ?>
+        <?php if (!empty($fancy_post_border_style)) : ?>
+            border-style: <?php echo esc_attr($fancy_post_border_style); ?>;
+        <?php endif; ?>
+        <?php if (!empty($fancy_post_border_width)) : ?>
+            border-width: <?php echo esc_attr($fancy_post_border_width); ?>;
+        <?php endif; ?>
+        <?php if (!empty($fpg_single_content_section_padding)) : ?>
+            padding: <?php echo esc_attr($fpg_single_content_section_padding); ?>;
+        <?php endif; ?>
+    }
+
+    /* Title Styles */
+    .rs-blog-layout-16-item .rs-content .title{
+        <?php if (!empty($fpg_title_order)) : ?>
+            order: <?php echo esc_attr($fpg_title_order); ?>;
+        <?php endif; ?>
+        <?php if (!empty($fpg_title_border_color)) : ?>
+            border-color: <?php echo esc_attr($fpg_title_border_color); ?>;
+        <?php endif; ?>
+        <?php if (!empty($fpg_title_border_style)) : ?>
+            border-style: <?php echo esc_attr($fpg_title_border_style); ?>;
+        <?php endif; ?>
+        <?php if (!empty($fpg_title_border_width)) : ?>
+            border-width: <?php echo esc_attr($fpg_title_border_width); ?>;
+        <?php endif; ?>
+    }
+    .rs-blog-layout-16-item .rs-content .title a{
+
+        <?php if (!empty($fpg_title_color)) : ?>
+            color: <?php echo esc_attr($fpg_title_color); ?>;
+        <?php endif; ?>
+        <?php if (!empty($fpg_title_font_size)) : ?>
+            font-size: <?php echo esc_attr($fpg_title_font_size); ?>px;
+        <?php endif; ?>
+        <?php if (!empty($fpg_title_font_weight)) : ?>
+            font-weight: <?php echo esc_attr($fpg_title_font_weight); ?>;
+        <?php endif; ?>
+    }
+
+    .rs-blog-layout-16-item .rs-content .title a:hover {
+
+        <?php if (!empty($fpg_title_hover_color)) : ?>
+            color: <?php echo esc_attr($fpg_title_hover_color); ?>;
+        <?php endif; ?>
+    }
+
+    /* Meta Data Styles */
+    .rs-blog-layout-16-item .rs-content .rs-meta .rs-author span,
+    .rs-blog-layout-16-item .rs-content .rs-meta .rs-date span,
+    .rs-blog-layout-16-item .rs-content .rs-meta .rs-date span i{
+        <?php if (!empty($fpg_meta_color)) : ?>
+            color: <?php echo esc_attr($fpg_meta_color); ?>;
+        <?php endif; ?>
+        <?php if (!empty($fpg_meta_size)) : ?>
+            font-size: <?php echo esc_attr($fpg_meta_size); ?>px;
+        <?php endif; ?>
+        <?php if (!empty($fpg_meta_font_weight)) : ?>
+            font-weight: <?php echo esc_attr($fpg_meta_font_weight); ?>;
+        <?php endif; ?>
+    }
+    .rs-blog-layout-16-item .rs-content .rs-meta{
+        <?php if (!empty($fpg_meta_order)) : ?>
+            order: <?php echo esc_attr($fpg_meta_order); ?>;
+        <?php endif; ?>      
+        <?php if (!empty($fpg_meta_padding)) : ?>
+            padding: <?php echo esc_attr($fpg_meta_padding); ?>;
+        <?php endif; ?>  
+        <?php if (!empty($fpg_meta_margin)) : ?>
+            margin: <?php echo esc_attr($fpg_meta_margin); ?>;
+        <?php endif; ?>  
+    }
+    .fpg-pagination ul.page-numbers{
+        <?php if (!empty($fpg_pagination_gap)) : ?>
+            gap: <?php echo esc_attr($fpg_pagination_gap); ?>;
+        <?php endif; ?> 
+    }
+    .fpg-pagination ul.page-numbers li .page-numbers {
+
+        <?php if (!empty($fpg_pagination_background)) : ?>
+            background-color: <?php echo esc_attr($fpg_pagination_background); ?>;
+        <?php endif; ?>
+        <?php if (!empty($fpg_pagination_color)) : ?>
+            color: <?php echo esc_attr($fpg_pagination_color); ?>;
+        <?php endif; ?>
+        <?php if (!empty($fpg_pagination_border_color)) : ?>
+            border-color: <?php echo esc_attr($fpg_pagination_border_color); ?>;
+        <?php endif; ?>
+        
+
+        <?php if (!empty($fpg_pagination_border_width)) : ?>
+            border-width: <?php echo esc_attr($fpg_pagination_border_width); ?>;
+        <?php endif; ?>
+
+        <?php if (!empty($fpg_pagination_border_style)) : ?>
+            border-style: <?php echo esc_attr($fpg_pagination_border_style); ?>;
+        <?php endif; ?>
+
+        <?php if (!empty($fpg_pagination_border_radius)) : ?>
+            border-radius: <?php echo esc_attr($fpg_pagination_border_radius); ?>;
+        <?php endif; ?>
+        <?php if (!empty($fpg_pagination_height)) : ?>
+            height: <?php echo esc_attr($fpg_pagination_height); ?>;
+        <?php endif; ?>
+
+        <?php if (!empty($fpg_pagination_width)) : ?>
+            width: <?php echo esc_attr($fpg_pagination_width); ?>;
+        <?php endif; ?>
+
+        <?php if (!empty($fpg_pagination_padding)) : ?>
+            padding: <?php echo esc_attr($fpg_pagination_padding); ?>;
+        <?php endif; ?>
+        <?php if (!empty($fpg_pagination_margin)) : ?>
+            margin: <?php echo esc_attr($fpg_pagination_margin); ?>;
+        <?php endif; ?> 
+    }
+    .fpg-pagination ul.page-numbers li .page-numbers:hover{
+        <?php if (!empty($fpg_pagination_hover_background)) : ?>
+            background-color: <?php echo esc_attr($fpg_pagination_hover_background); ?>;
+        <?php endif; ?>
+        <?php if (!empty($fpg_pagination_hover_color)) : ?>
+            color: <?php echo esc_attr($fpg_pagination_hover_color); ?>;
+        <?php endif; ?>
+        <?php if (!empty($fpg_pagination_hover_border_color)) : ?>
+            border-color: <?php echo esc_attr($fpg_pagination_hover_border_color); ?>;
+        <?php endif; ?>
+    }
+    .fpg-pagination ul.page-numbers li .page-numbers.current{
+        <?php if (!empty($fpg_pagination_active_background)) : ?>
+            background-color: <?php echo esc_attr($fpg_pagination_active_background); ?>;
+        <?php endif; ?>
+        <?php if (!empty($fpg_pagination_active_color)) : ?>
+            color: <?php echo esc_attr($fpg_pagination_active_color); ?>;
+        <?php endif; ?>
+        <?php if (!empty($fpg_pagination_active_border_color)) : ?>
+            border-color: <?php echo esc_attr($fpg_pagination_active_border_color); ?>;
+        <?php endif; ?>
+    }
+
+</style>
 <?php
 $grid9 = ob_get_clean();
 ?>
